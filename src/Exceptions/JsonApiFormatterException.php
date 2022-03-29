ad028ed7734ab96e69b65f941faaa7a88161bb9c
elb98rm <rick@floor9design.com>
2022-03-29 14:39:26 +0100

    Fixed some incorrect comments. Composer update.

--- src/Exceptions/JsonApiFormatterException.php
+++ src/Exceptions/JsonApiFormatterException.php
@@ -24,9 +24,6 @@ use Exception;
 /**
  * Class JsonApiFormatterException
  *
- * Class to offer methods/properties to format items ready for a Json Api request.
- * These are set to the v1.0 specification, defined at https://jsonapi.org/format/
- *
  * @category  None
  * @package   Floor9design\JsonApiFormatter\Models
  * @author    Rick Morice <rick@floor9design.com>
@@ -34,8 +31,6 @@ use Exception;
  * @license   MIT
  * @version   0.5.0
  * @link      https://www.floor9design.com
- * @link      https://jsonapi.org/
- * @link      https://jsonapi-validator.herokuapp.com/
  * @since     File available since pre-release development cycle
  * @see       https://jsonapi.org/format/
  */
